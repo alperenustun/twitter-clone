9032bfb91fe7212511654d84d2ea82bd498b03a7
ShiftyShifter <58483864+ShiftyShifter@users.noreply.github.com>
2022-10-18 23:46:14 +0300

    init

---
+++ data.js
@@ -0,0 +1,58 @@
+export const tweetsData = [   
+    {
+        handle: `@NFTManiac4324244 💎`,
+        profilePic: `images/nft.jpg`,
+        likes: 27,
+        retweets: 10,
+        tweetText: `Drop your NFTS 💰💰💰 
+            Planning to buy some tonight!!`,
+        replies: [],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '4b161eee-c0f5-4545-9c4b-8562944223ee',
+    },    
+    {
+        handle: `@Elon ✅`,
+        profilePic: `images/musk.png`,
+        likes: 6500,
+        retweets: 234,
+        tweetText: `I need volunteers for a one-way mission to Mars 🪐. No experience necessary🚀`,
+        replies: [
+                  {
+                handle: `@TomCruise ✅`,
+                profilePic: `images/tcruise.png`,
+                tweetText: `Yes! Sign me up! 😎🛩`,
+            },
+                  {
+                handle: `@ChuckNorris ✅`,
+                profilePic: `images/chucknorris.jpeg`,
+                tweetText: `I went last year😴`,
+            },
+        ],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '3c23454ee-c0f5-9g9g-9c4b-77835tgs2',
+    },
+        {
+        handle: `@DonaldJTrump`,
+        profilePic: `images/trump.png`,
+        likes: 547,
+        retweets: 22,
+        tweetText: `Make Tweety great again!`,
+        replies: [
+            {
+                handle: `@toxiC435345`,
+                profilePic: `images/overflow.png`,
+                tweetText: `Why don't you buy it and make great?`,
+            },
+            {
+                handle: `@trueLovve24`,
+                profilePic: `images/love.png`,
+                tweetText: `It is already great ❤️`,
+            },
+        ],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '8hy671sff-c0f5-4545-9c4b-1237gyys45',
+    },     
+]
